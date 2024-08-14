5a41f701998dc9ed9f9cc73917b567c461df8fdb
Siddhant <126678613+siddhant-tambatkar@users.noreply.github.com>
2024-08-14 14:13:29 +0530

    Create simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# This script calculates simple interest given principal,
+# annual rate of interest and time period in years.
+# Do not use this in production. Sample purpose only.
+# Author: Upkar Lidder (IBM)
+# Additional Authors:
+# <siddhant-tambatkar>
+
+echo "Enter the principal:"
+read p
+echo "Enter rate of interest per year:"
+read r
+echo "Enter time period in years:"
+read t
+
+s=`expr $p \* $t \* $r / 100`
+echo "The simple interest is: $s"
